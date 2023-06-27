d592f301f4359a9f3622567ddbf5c3cc35e8e5ac
marjanhasan <moonmarjanhasan@gmail.com>
2023-06-28 03:18:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Intro from "./components/Intro";
+import Portfolio from "./components/Portfolio";
+import Timeline from "./components/Timeline";
+
+const App = () => {
+  return (
+    <div>
+      <Intro />
+      <Portfolio />
+      <Timeline />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Intro.jsx
@@ -0,0 +1,9 @@
+const Intro = () => {
+  return (
+    <div>
+      <h1>intro</h1>
+    </div>
+  );
+};
+
+export default Intro;
