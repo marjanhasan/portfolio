f96444c7807674ab75c5ef4ebfc0de1aaf185378
marjanhasan <moonmarjanhasan@gmail.com>
2023-06-29 02:19:36 +0600

    intro section done

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,19 @@
 const Intro = () => {
   return (
-    <div>
-      <h1>intro</h1>
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
+        Marjan Hasan Moon
+      </h1>
+      <p className="text-base md:text-xl mb-3 font-medium">
+        Softwar Engineer & Web Developer
+      </p>
+      <p className="text-sm max-w-xl mb-6 font-bold">
+        Passionate Full-Stack Developer proficient in JavaScript, ReactJS, and
+        REST API, with expertise in HTML5, CSS3, and responsive design. Seeking
+        challenging opportunities to utilize NodeJs, ExpressJs, and MongoDB
+        skills to develop scalable web applications that deliver exceptional
+        user experiences.
+      </p>
     </div>
   );
 };
